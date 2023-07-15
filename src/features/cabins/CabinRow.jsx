8eac1451361d53283662645a2329eecc1f72380a
Colin Huber <cbhuber@gmail.com>
2023-07-14 20:29:08 -0600

    feat: displaying notification when deleting cabin.

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { formatCurrency } from "./../../utils/helpers";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCabin } from "../../services/apiCabins";
+import { toast } from "react-hot-toast";
 
 const TableRow = styled.div`
   display: grid;
@@ -63,14 +64,14 @@ function CabinRow({ cabin }) {
   const { isLoading: isDeleting, mutate } = useMutation({
     mutationFn: deleteCabin,
     onSuccess: () => {
-      alert(`Cabin ${cabinId} successfully deleted.`);
+      toast.success(`Cabin ${cabinId} successfully deleted.`);
 
       // Invalidate the queries to trigger a refresh
       queryClient.invalidateQueries({
         queryKey: ["cabins"],
       });
     },
-    onError: (err) => alert(err.message),
+    onError: (err) => toast.error(err.message),
   });
 
   return (
