46d85faff060ca078f3c4fdcab2cefd754a7ecc4
Colin Huber <cbhuber@gmail.com>
2023-07-15 22:52:41 -0600

    env: remove unused code.

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,20 +2,14 @@ import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
-import { getCabins } from "../services/apiCabins";
 import AddCabin from "../features/cabins/AddCabin";
 
 function Cabins() {
-  // useEffect(function () {
-  //   getCabins().then((data) => console.log(data));
-  // }, []);
-
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
         <p>Filter / Sort</p>
-        {/* <img src="https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg" /> */}
       </Row>
 
       <Row>
