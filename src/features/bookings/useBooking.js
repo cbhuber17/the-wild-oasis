0ea9d3ae57df997cecfaf78190e268b29bdaf97b
Colin Huber <cbhuber@gmail.com>
2023-07-16 20:43:13 -0600

    feat: single booking details page.

---
+++ src/features/bookings/useBooking.js
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import { getBooking } from "../../services/apiBookings";
+
+export function useBooking() {
+  const { bookingId } = useParams();
+
+  const {
+    isLoading,
+    data: booking,
+    error,
+  } = useQuery({
+    queryKey: ["booking", bookingId],
+    queryFn: () => getBooking(bookingId),
+    retry: false, // ReactQuery will try to fetch 3 times, disable that here
+  });
+
+  return { isLoading, error, booking };
+}
