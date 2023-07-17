3218dffbb2bc88c342b9a37884d701caa5c67760
Colin Huber <cbhuber@gmail.com>
2023-07-17 15:25:39 -0600

    env: updating code comment.

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -12,6 +12,7 @@ export function useLogin() {
     onSuccess: (user) => {
       // After logging in, its not necessary to refetch the user via another API call
       // Put user in ReactQuery cache after logging in
+      // Do not use queryClient.setQueriesData()
       queryClient.setQueryData(["user"], user.user);
       navigate("/dashboard", { replace: true }); // Replace prevents browser back button; erases history
     },
